c1a8ad6e9be4ee7e8479d738055ddffa40712222
Dawson Skirvin <dskirvinc@gmail.com>
2020-09-08 02:22:51 -0400

    reworking of NumberPrinter.print

--- src/literateProgramming/NumberPrinter.java
+++ src/literateProgramming/NumberPrinter.java
@@ -3,31 +3,61 @@ package literateProgramming;
 public class NumberPrinter {
     private final int colsPerPage;
     private final int rowsPerPage;
+    private int pageNumber;
+    private int pageOffset;
+    private int numberOfNumbers;
+    private int[] numbers;
 
     public NumberPrinter(int colsPerPage, int rowsPerPage) {
         this.colsPerPage = colsPerPage;
         this.rowsPerPage = rowsPerPage;
     }
 
-    void print(int numPrimes, int[] primes) {
-        int pageNumber = 1;
-        int pageOffset = 1;
-        while (pageOffset <= numPrimes) {
-            System.out.print("The First ");
-            System.out.print(Integer.toString(numPrimes));
-            System.out.print(" Prime Numbers --- Page ");
-            System.out.print(Integer.toString(pageNumber));
-            System.out.println("\n");
-            for (int rowOffset = pageOffset; rowOffset <= pageOffset + rowsPerPage - 1; rowOffset++) {
-                for (int col = 0; col <= colsPerPage - 1; col++)
-                    if (rowOffset + col * rowsPerPage <= numPrimes)
-                        System.out.printf("%10d", primes[rowOffset + col * rowsPerPage]);
-                System.out.println();
-            }
-            System.out.println("\f");
-            pageNumber++;
-            pageOffset += rowsPerPage * colsPerPage;
+    void print(int[] numbers) {
+        initialize(numbers);
+        while (needToPrintMore()) {
+            printHeader();
+            printNumbersOnPage();
+            moveToNextPage();
         }
+    }
+
+    private boolean needToPrintMore() {
+        return pageOffset <= numberOfNumbers;
+    }
+
+    private void initialize(int[] numbers) {
+        this.numbers = numbers;
+        pageNumber = 1;
+        pageOffset = 1;
+        numberOfNumbers = this.numbers.length - 1;
+    }
+
+    private void moveToNextPage() {
+        System.out.println("\f");
+        pageNumber++;
+        pageOffset += rowsPerPage * colsPerPage;
+    }
+
+    private void printNumbersOnPage() {
+        for (int row = 0; row < rowsPerPage; row++) {
+            for (int col = 0; col < colsPerPage; col++)
+                printNumberAt(pageOffset + row + col * rowsPerPage);
+            System.out.println();
+        }
+    }
+
+    private void printNumberAt(int index) {
+        if (index <= this.numberOfNumbers)
+            System.out.printf("%10d", this.numbers[index]);
+    }
+
+    private void printHeader() {
+        System.out.print("The First ");
+        System.out.print(Integer.toString(numberOfNumbers));
+        System.out.print(" Prime Numbers --- Page ");
+        System.out.print(Integer.toString(pageNumber));
+        System.out.println("\n");
     }
 
 }
