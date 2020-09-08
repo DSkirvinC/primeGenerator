a25bc802c7ea782a7213434fb89f447604341ebf
Dawson Skirvin <dskirvinc@gmail.com>
2020-09-08 18:45:09 -0400

    remove unneeded fields from Numberprinter

--- src/literateProgramming/NumberPrinter.java
+++ src/literateProgramming/NumberPrinter.java
@@ -4,8 +4,6 @@ public class NumberPrinter {
     private final int colsPerPage;
     private final int rowsPerPage;
     private int pageNumber;
-    private int pageOffset;
-    private int numberOfNumbers;
     private int[] numbers;
 
     public NumberPrinter(int colsPerPage, int rowsPerPage) {
@@ -23,41 +21,45 @@ public class NumberPrinter {
     }
 
     private boolean needToPrintMore() {
-        return pageOffset <= numberOfNumbers;
+        return getPageOffset() <= getNumberOfNumbers();
     }
 
     private void initialize(int[] numbers) {
         this.numbers = numbers;
         pageNumber = 1;
-        pageOffset = 1;
-        numberOfNumbers = this.numbers.length - 1;
     }
 
     private void moveToNextPage() {
         System.out.println("\f");
         pageNumber++;
-        pageOffset += rowsPerPage * colsPerPage;
     }
 
     private void printNumbersOnPage() {
         for (int row = 0; row < rowsPerPage; row++) {
             for (int col = 0; col < colsPerPage; col++)
-                printNumberAt(pageOffset + row + col * rowsPerPage);
+                printNumberAt(getPageOffset() + row + col * rowsPerPage);
             System.out.println();
         }
     }
 
     private void printNumberAt(int index) {
-        if (index <= this.numberOfNumbers)
+        if (index <= this.getNumberOfNumbers())
             System.out.printf("%10d", this.numbers[index]);
     }
 
     private void printHeader() {
         System.out.print("The First ");
-        System.out.print(Integer.toString(numberOfNumbers));
+        System.out.print(Integer.toString(getNumberOfNumbers()));
         System.out.print(" Prime Numbers --- Page ");
         System.out.print(Integer.toString(pageNumber));
         System.out.println("\n");
     }
 
+    public int getPageOffset() {
+        return (pageNumber - 1) * rowsPerPage * colsPerPage + 1;
+    }
+
+    public int getNumberOfNumbers() {
+        return numbers.length - 1;
+    }
 }
