b367df93392e676c17e0be24c16a27deacad17b4
Dawson Skirvin <dskirvinc@gmail.com>
2020-09-08 19:03:52 -0400

    refactor to turn pageNumber into a local variable to print

--- src/literateProgramming/NumberPrinter.java
+++ src/literateProgramming/NumberPrinter.java
@@ -3,7 +3,6 @@ package literateProgramming;
 public class NumberPrinter {
     private final int colsPerPage;
     private final int rowsPerPage;
-    private int pageNumber;
     private int[] numbers;
 
     public NumberPrinter(int colsPerPage, int rowsPerPage) {
@@ -12,32 +11,24 @@ public class NumberPrinter {
     }
 
     void print(int[] numbers, String title) {
-        initialize(numbers);
-        while (needToPrintMore()) {
-            printHeader(title);
-            printNumbersOnPage();
-            moveToNextPage();
+        this.numbers = numbers;
+        int pageNumber = 1;
+        while (needToPrintMore(pageNumber)) {
+            printHeader(title, pageNumber);
+            printNumbersOnPage(pageNumber);
+            System.out.println("\f");
+            pageNumber++;
         }
     }
 
-    private boolean needToPrintMore() {
-        return getPageOffset() <= getNumberOfNumbers();
+    private boolean needToPrintMore(int pageNumber) {
+        return getPageOffset(pageNumber) <= getNumberOfNumbers();
     }
 
-    private void initialize(int[] numbers) {
-        this.numbers = numbers;
-        pageNumber = 1;
-    }
-
-    private void moveToNextPage() {
-        System.out.println("\f");
-        pageNumber++;
-    }
-
-    private void printNumbersOnPage() {
+    private void printNumbersOnPage(int pageNumber) {
         for (int row = 0; row < rowsPerPage; row++) {
             for (int col = 0; col < colsPerPage; col++)
-                printNumberAt(getPageOffset() + row + col * rowsPerPage);
+                printNumberAt(getPageOffset(pageNumber) + row + col * rowsPerPage);
             System.out.println();
         }
     }
@@ -47,11 +38,11 @@ public class NumberPrinter {
             System.out.printf("%10d", this.numbers[index]);
     }
 
-    private void printHeader(String title) {
+    private void printHeader(String title, int pageNumber) {
         System.out.printf("%s--- Page %d\n%n", title, pageNumber);
     }
 
-    public int getPageOffset() {
+    public int getPageOffset(int pageNumber) {
         return (pageNumber - 1) * rowsPerPage * colsPerPage + 1;
     }
 
